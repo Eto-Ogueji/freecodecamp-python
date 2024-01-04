8a69804004374148309a16ce52ff11de09b51ef2
EtoOgueji <etoogueji@gmail.com>
2024-01-04 22:18:19 +0100

    if statemant sample

---
+++ 15-simple_if.py
@@ -0,0 +1,13 @@
+condition = True
+name =  "Eto"
+if condition == True:
+    print("The condition was True")
+
+elif name == "Roger":
+    print("Hello Roger")
+
+elif name == "Syd":
+    print("Hello Syd")
+
+elif name == "Flavio":
+    print("Hello Flavio")
