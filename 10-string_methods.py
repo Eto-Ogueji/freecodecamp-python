5f4bfb586d50454ebfc13134c16b454d97fda106
EtoOgueji <etoogueji@gmail.com>
2024-01-04 22:16:52 +0100

    string methods

---
+++ 10-string_methods.py
@@ -0,0 +1,13 @@
+name = "Etomchukwu"
+
+# make a copy of the string all in lowercase
+print(name.lower()) # etomchukwu
+
+# but doesn't affect the original string
+print(name) # Etomchukwu
+
+print("to" in name) # True
+
+print(name[0]) # 'E'
+
+print(name[0:3]) # 'Eto'
